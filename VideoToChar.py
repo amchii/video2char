4eda2622e20b2b055111fb224db5ba9cf656a393
Exmlyshy <caoheng0420@gmail.com>
2019-02-12 10:43:55 +0800

    加一个自动缩小选项

--- VideoToChar.py
+++ VideoToChar.py
@@ -31,7 +31,7 @@ def auto_thumb(img):
 def toChar_print(img, w_thumb=1.0, h_thumb=1.0):
     height, width = img.shape[:2]
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    if w_thumb == h_thumb == 1:
+    if w_thumb == h_thumb == 1 and AUTO_THUMB == True:
         img_gray_resize = auto_thumb(img_gray)
     else:
         img_gray_resize = cv2.resize(
@@ -70,7 +70,7 @@ def Video2Char(file):
         print('非支持的文件...')
 
 if __name__ == '__main__':
-    print('------------建议按分辨率将宽度缩小至100+,高缩小倍数为宽2倍(默认宽度为%s)-------------' % D_WIDTH)
+    print('------------建议按分辨率将宽度缩小至百级像素,高缩小倍数为宽2倍,缩小控制台输出字体效果更佳(默认宽度为%s)-------------' % D_WIDTH)
     print('当前路径：%s' % os.path.abspath('.'))
     while True:
         file_path = input('请输入图片或视频路径：')
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     if img is not None:
         print('当前图片尺寸为宽：%s 高：%s' % (img.shape[1], img.shape[0]))
         showImage(img, 1)
-        flag = input('是否缩小比例(y/n)?\n:')
+        flag = input('是否手动缩小比例(y/n)?\n:')
         if flag.lower() == 'y':
             print('缩小倍数：')
             w_thumb = float(input('宽：'))
--- config.py
+++ config.py
@@ -4,6 +4,9 @@
 
 #由密到疏字符组合
 ASCII_CHAR = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+#自动缩小至默认宽度
+AUTO_THUMB = True
 #默认宽度
 D_WIDTH=140 
 
